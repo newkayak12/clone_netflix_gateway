e9aad513fe892ae9fd279c978144b122449a2f10
newkayak12 <newkayak12@gmail.com>
2023-05-17 21:46:02 +0900

    route

---
+++ src/main/java/com/netflix_clone/gateway/RouteConfig.java
@@ -0,0 +1,37 @@
+package com.netflix_clone.gateway;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator gateWayRoute(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(
+                        predicate -> predicate.path("/api/v1/board/**")
+                                              .uri("lb://NETFLIX-CLONE-BOARD-SERVICE")
+                )
+                .route(
+                        predicate -> predicate.path("/api/v1/user/**")
+                                .uri("lb://NETFLIX-CLONE-USER-SERVICE")
+                )
+                .route(
+                        predicate -> predicate.path("/api/v1/movie/**")
+                                .uri("lb://NETFLIX-CLONE-USER-SERVICE")
+                )
+                .build();
+    }
+}
+//    route(r -> r.path("/first-service/**")
+//        .uri("http://localhost:8081"))
+//        //second-service
+//        .route(r -> r.path("/second-service/**")
+//        .filters(f -> f.addResponseHeader("second-request", "second-request-header")
+//        .addResponseHeader("second-response", "second-response-header"))
+//        .uri("http://localhost:8082"))
+//        .build();
